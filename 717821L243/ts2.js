19c258b678dfa070de40f5462213ae25708b111b
ROOPIKA SREE S <136248960+21L243@users.noreply.github.com>
2023-08-27 23:34:30 +0530

    Add files via upload

---
+++ 717821L243/ts2.js
@@ -0,0 +1,19 @@
+function BC()
+     {
+      
+      window.location.href = "Book Collections.html";
+      
+     }
+     function BC2()
+     {
+      
+      window.location.href = "details.html";
+      
+     }
+     function BC1()
+     {
+      
+      window.location.href = "author.html";
+      
+     }
+    
